2c7168edc06038256831aac203fe39bc05da7d2d
Rangel Diaz Luis Fernando <60003208+Luis150228@users.noreply.github.com>
2025-09-04 12:34:54 -0600

    Crear tsconfig.tsx

---
+++ tsx/tsconfig.tsx
@@ -0,0 +1,10 @@
+{
+  "compilerOptions": {
+    "strict": true,
+    "noImplicitAny": true,
+    "module": "ESNext",
+    "moduleResolution": "bundler",
+    "jsx": "react-jsx",
+    "types": ["vite/client"]
+  }
+}
